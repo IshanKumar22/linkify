fae6eca467c800f884fec39812725ce57a7c57e4
IshanKumar22 <101940131+IshanKumar22@users.noreply.github.com>
2023-04-14 15:09:30 +0000

    Updated

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def shorten_url():
         Takes the long url, shortens it, saves it to the url dictionary and renders the shorten_url.html file
     """
     long_url = request.form["long_url"]
-    host_url = request.form["url"]
+    host_url = request.form["hostname"]
     hashed = hashlib.md5(long_url.encode()).hexdigest()[:8]
-    urls[hash] = long_url
+    urls[hashed] = long_url
     short_url = host_url + hashed
     return render_template("shorten_url.html", short_url=short_url)
 
