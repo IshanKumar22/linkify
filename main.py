36f904c05b5211ca46118032658940b9d9e18eb0
IshanKumar22 <101940131+IshanKumar22@users.noreply.github.com>
2023-04-14 14:14:46 +0000

    Added functionality

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
-from flask import Flask, render_template, request, redirect
 import hashlib
-import ssl
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ae93ec19909d56777c37c55dd9be07ab"
 urls = {}
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain("cert.pem", "key.pem")
 
 @app.route("/")
 def index():
+    """
+        Renders the index.html file
+    """
     return render_template("index.html")
+
 
 @app.route("/shorten_url", methods=["POST"])
 def shorten_url():
+    """
+        Takes the long url, shortens it, saves it to the url dictionary and renders the shorten_url.html file
+    """
     long_url = request.form["long_url"]
-    hash = hashlib.md5(long_url.encode()).hexdigest()[:8]
+    host_url = request.form["url"]
+    hashed = hashlib.md5(long_url.encode()).hexdigest()[:8]
     urls[hash] = long_url
-    short_url = request.host_url + hash
+    short_url = host_url + hashed
     return render_template("shorten_url.html", short_url=short_url)
 
-@app.route("/<hash>")
-def redirect_url(hash):
-    long_url = urls.get(hash)
+
+@app.route("/<hashed>")
+def redirect_url(hashed: str):
+    """
+        Redirects the short url to the long url
+
+        :params hashed: The code for the short url
+    """
+    long_url = urls.get(hashed)
     if long_url:
         return redirect(long_url)
     else:
         return render_template("404.html")
 
+
 if __name__ == "__main__":
-    app.run(host="192.168.0.108", port=80, debug=True, ssl_context=context)
+    app.run(host="0.0.0.0", port=8081, debug=True)
